484a80b65d9dab772e4e41165b39ba423eea0228
Gauri Joshi <gauri728@gmail.com>
2022-08-07 09:07:36 -0700

    initial migrations and model relationships

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Hospital do
   describe 'relationships' do
-    it { should have_many(:doctors) }
+    it {should have_many :doctors}
+    it { should have_many(:doctor_patients).through(:doctors) }
+    it { should have_many(:patients).through(:doctor_patients)}
   end
 end
