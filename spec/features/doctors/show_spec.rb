f89a153e4b2f63890299dd9febea469bde95a743
Gauri Joshi <gauri728@gmail.com>
2022-08-07 10:36:25 -0700

    finishes user stories 1-3

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -67,4 +67,48 @@ RSpec.describe "Doctors show page" do
       expect(page).to have_content("Sam Smith")
       expect(page).to_not have_content("Sally Omally")
    end
+
+   it "has a button to remove a patient from the doctor's caseload" do 
+      grey_sloan = Hospital.create!(name: "Grey Sloan Memorial")
+
+      mer = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Boston University", hospital_id: grey_sloan.id)
+      yang = Doctor.create!(name: "Cristina Yang", specialty: "Cardiology", university: "Stanford University", hospital_id: grey_sloan.id)
+      karev = Doctor.create!(name: "Alex Karev", specialty: "Pediatric Surgery", university: "UCLA", hospital_id: grey_sloan.id)
+
+      patient1 = Patient.create!(name: "Denny Duquette", age: 37)
+      patient2 = Patient.create!(name: "Sam Smith", age: 38)
+      patient3 = Patient.create!(name: "Sally Omally", age: 39)
+
+      doc_pat1 = DoctorPatient.create!(doctor_id: mer.id, patient_id: patient1.id)
+      doc_pat2 = DoctorPatient.create!(doctor_id: mer.id, patient_id: patient2.id)
+      doc_pat3 = DoctorPatient.create!(doctor_id: yang.id, patient_id: patient3.id)
+
+      visit "/doctors/#{mer.id}"
+
+      expect(page).to have_button("Remove #{patient1.name}")
+      expect(page).to have_button("Remove #{patient2.name}")
+      expect(page).to_not have_button("Remove #{patient3.name}")
+   end
+
+   it "takes the Doctor back to their show page where that patient is no longer listed" do 
+      grey_sloan = Hospital.create!(name: "Grey Sloan Memorial")
+
+      mer = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Boston University", hospital_id: grey_sloan.id)
+      yang = Doctor.create!(name: "Cristina Yang", specialty: "Cardiology", university: "Stanford University", hospital_id: grey_sloan.id)
+      karev = Doctor.create!(name: "Alex Karev", specialty: "Pediatric Surgery", university: "UCLA", hospital_id: grey_sloan.id)
+
+      patient1 = Patient.create!(name: "Denny Duquette", age: 37)
+      patient2 = Patient.create!(name: "Sam Smith", age: 38)
+      patient3 = Patient.create!(name: "Sally Omally", age: 39)
+
+      doc_pat1 = DoctorPatient.create!(doctor_id: mer.id, patient_id: patient1.id)
+      doc_pat2 = DoctorPatient.create!(doctor_id: mer.id, patient_id: patient2.id)
+      doc_pat3 = DoctorPatient.create!(doctor_id: yang.id, patient_id: patient3.id)
+
+      visit "/doctors/#{mer.id}"
+
+      click_button "Remove Denny Duquette"
+      expect(current_path).to eq("/doctors/#{mer.id}")
+      expect(page).to_not have_content("Denny Duquette")
+   end
 end
